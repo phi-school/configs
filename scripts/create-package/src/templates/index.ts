e75fce932e44fca27838f9030c2b07d2db83dd77
theurgi <58859663+theurgi@users.noreply.github.com>
2023-09-30 17:48:19 -0400

    feat: init monorepo [skip ci]

---
+++ scripts/create-package/src/templates/index.ts
@@ -0,0 +1,4 @@
+export { generatePackageJson, type PackageJsonOptions } from './package-json'
+export { generateTsConfig } from './tsconfig'
+export { generateTsupConfig } from './tsup'
+export { generateVitestConfig } from './vitest'
